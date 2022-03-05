8c26a1b4128af39a2da25e78ff3f32b9d460ba04
gorkemetem <gorkemetem98@gmail.com>
2022-03-06 00:26:08 +0300

    06.03.2022

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleUI
 
             foreach (var product in productManager.GetByUnitPrice(40,100))
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName); 
             }
             
         }
