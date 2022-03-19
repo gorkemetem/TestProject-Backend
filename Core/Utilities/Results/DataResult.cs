06e1997b82572640d57dda2847e13f4cd6316967
gorkemetem <gorkemetem98@gmail.com>
2022-03-19 18:16:20 +0300

    Day10_End

--- Core/Utilities/Results/DataResult.cs
+++ Core/Utilities/Results/DataResult.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace Core.Utilities.Results
 {
-    public class DataResult
+    public class DataResult<T> : Result, IDataResult<T>
     {
+        public DataResult(T data, bool succes, string message):base(succes,message)
+        {
+            Data = data;
+        }
+
+        public DataResult(T data, bool success):base(success)
+        {
+            Data = data;
+        }
+        public T Data { get; }
     }
 }
