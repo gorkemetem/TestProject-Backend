4e7d7001b78b8e97aa1dda350875c5f1f1f05756
gorkemetem <gorkemetem98@gmail.com>
2022-03-22 20:55:35 +0300

    Startup.cs updated - Day11_2

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
-﻿using Entities.Concrete;
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
@@ -9,15 +12,19 @@ namespace WebAPI.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         [HttpGet]
         public List<Product> Get()
         {
-            return new List<Product>()
-            {
-                new Product { ProductId = 1, ProductName = "Elma" },
-                new Product { ProductId = 2, ProductName = "Armut" },
-            };
             
+            var result = _productService.GetAll();
+            return result.Data;
         }
     }
 }
